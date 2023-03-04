245d8ec11d5ff674321214336432189e0929dab5
hzy <andrew_hzy@163.com>
2023-03-04 17:02:02 +0800

    add decorator codes

---
+++ src/headfirst/designpatterns/decorator/starbuzz/starbuzz/DarkRoast.java
@@ -0,0 +1,9 @@
+package headfirst.designpatterns.decorator.starbuzz.starbuzz;
+
+public class DarkRoast extends Beverage {
+    public DarkRoast() {
+        name = "Dark Roast Coffee";
+        cost = 0.99;
+    }
+}
+
